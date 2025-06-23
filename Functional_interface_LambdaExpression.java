9095699efc1ea5705c2b02f7baf9a1c784690771
Subodh Nandi <subodhnandi59428@gmail.com>
2025-06-23 10:57:35 +0530

    Add files via upload

---
+++ Functional_interface_LambdaExpression.java
@@ -0,0 +1,10 @@
+package com.subodh.new_features;
+
+public class Functional_interface_LambdaExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
