0e429922a687ad25a976f327effd96af07d1a4e6
Subodh Nandi <subodhnandi59428@gmail.com>
2025-06-25 10:54:16 +0530

    Add files via upload

---
+++ Predefined_Functional_interface.java
@@ -0,0 +1,48 @@
+package com.subodh.new_features;
+/*Functional Interface(FI)
+ * FI->Representing Math Opearation
+ * A math operation requires inputs and returns output
+ * 
+ * FI must be:
+ * 	1.Parameterized FI
+ * 	2.Non-void FI
+ * 	3.parameterized and Non-void FI
+ * 	4.parameterized and Boolean return type FI
+ * 
+ *java provides 43 predefined functional interface. 
+ * 
+ * FIs are broadly divided into 4 types
+ * 	1.consumer<T>
+ * 		-One Parameter method FI for consuming Passed Argument.
+ * 	2.Supplier<T>
+ * 		-No parameter and non-void method FI for returning value or objects.
+ * 	3.function<T,R>
+ * 		-One parameter and non void method for taking input and return result.
+ * 	4.Predicate<T>
+ * 		-One parameter and boolean return type method FI to verify given argument.
+ * 
+ * The additional interfaces for twp arguments.
+ * 	1.Biconsumer<T,U>
+ * 		-Two Parameter method FI for consuming Passed Argument.
+ * 	2.BooleanSupplier
+ * 		
+ * 	3.BiFunction<T,U,R>
+ * 		-Two parameter and non void method for taking input and return result.
+ * 	4.BiPredicate<T,U>
+ * 		-Two parameter and boolean return type method FI to verify given argument.
+ * 
+ * 	5.UnaryOperator
+ * 	6.BinaryOperator
+ * 
+ * refer complete list PD FIs from java 8v API documetation all FIs are provided in the
+ * package java.util.function
+ */
+
+public class Predefined_Functional_interface {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
